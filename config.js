5e3c61a03d6986650c7ce99ee287a8aa49f9f4fc
altweijie <94088278+altweijie@users.noreply.github.com>
2022-01-06 07:45:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,28 +1,36 @@
-
 module.exports = {
-    "registercommands" : false, //Write True If You Are Launching The Bot First Time
-     "token": "", //Your Super Secret Bot Token
-    "imageapi": "", //Your Amethyste Api You can get it from https://api.amethyste.moe/
-    "ownerID": [], //Your Discord User ID
-"prefix": "", //Your Bot's Prefix
+    "registercommands" : false,
+     "token": "OTI4MTA2NzMxNzYwOTQzMTA2.YdT8wA.ovqAg5B-4TrXf16ScfjBRX3IQJM",
+    "imageapi": "bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d",
+    "ownerID": ["897709712047505408"],
+"prefix": "+",
 "chat": {
-        "url": "",
-        "bid": "",
-        "key": "",
-        "uid": ""
-    }, // You can get This Things from https://brainshop.ai
-"api": "", // Your Youtube Api
-"youtubeAPI": "", // Your Youtube Api
-mainprefix: "",  // Again Your Prefix
-"owner": "G U D B O Y", // Owner Name
+        "url": "http://api.brainshop.ai/get?bid=155777&key=O0Rla6COZJ8XSGPJ&uid=[uid]&msg=[msg]",
+        "bid": "155777",
+        "key": "O0Rla6COZJ8XSGPJ",
+        "uid": "nothing."
+    },
+    "bid": "897709712047505408",
 
+"api": "AIzaSyAAg3GHxipuyDz7KsCAv434yoFT56TR9LQ",
+"youtubeAPI": "AIzaSyAAg3GHxipuyDz7KsCAv434yoFT56TR9LQ",
+mainprefix: "+",
+"owner": "Ano",
+"mongourl": "mongodb+srv://099:099@cluster0.kjeqb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+"secret": "TKDHwzyl2iieCPgq9mp4O5gttwqEOm1c",
+ "dashboardURL": "if you have",
+defaultjoinmessage: "{user} Joined Invited By {Inviter} (Inviter Invites: {inv})",
+defaultleavemessage: "{user} Left, Invited by {inviter}",
   basiclang: "en", //The basic language of the bot, "fr" for French and "en" for English
     embeds: {
         color: "BLUE", //Embed color (in English)
         footers: "GIVEAWAY :tada: :tada:" //Embed footer
     },
 
-   
+    start: {
+        loading: "LOADING XD", //Loading status
+        activity: "BRUH" //Status
+    },
 
     events: {
         addcolor: "GREEN", //The color of the event add (in English)
@@ -34,7 +42,7 @@ mainprefix: "",  // Again Your Prefix
     grole: "Giveaway Manager", //If the member doesn't have permission to handle messages he can still use the giveaways commands if he has the role configured right here
 
     auth: {
-        support: "XXX", //The link of your Discord server
+        support: "https://discord.gg/EVrnNxYdEN", //The link of your Discord server
         dperms: "8" //The permissions that the bot asks on we want to add it on a Discord server (8 = moderator)
-    }, 
+    },
 } 
