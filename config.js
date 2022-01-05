8523cbdcb5a7524b8f257bc1c14bbfbd029803af
altweijie <94088278+altweijie@users.noreply.github.com>
2022-01-06 07:47:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "registercommands" : false,
-     "token": "OTI4MTA2NzMxNzYwOTQzMTA2.YdT8wA.ovqAg5B-4TrXf16ScfjBRX3IQJM",
+     "token": "BOT_TOKEN",
     "imageapi": "bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d",
     "ownerID": ["897709712047505408"],
 "prefix": "+",
